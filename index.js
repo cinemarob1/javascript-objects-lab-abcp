4ae1e8ba8cd2a501a9d27327d23319679e1e388a
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-11 23:38:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = {base: "rice"};
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key]=value;
-  return object;
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObject = object[key]=value;
+  return newObject;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
