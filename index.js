1e90a9de103fe9326c7852d9d85d2cb110a1c456
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-11 23:37:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key]=value;
   return object;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value;
+  return object;
+}
