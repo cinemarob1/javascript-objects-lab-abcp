c22d9c05cc1bd0cfaf00c11b652bbf5576cd4700
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-11 23:44:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {base: "rice"};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign({},{[]})
+  Object.assign({},{object},{key:})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
